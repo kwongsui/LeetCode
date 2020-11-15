3f88bc6a3d85d1816edb9079b7d20f0e35b1c6f2
kwongsui <hwangkwongsui@gmail.com>
2020-11-16 07:22:08 +0800

    ADD: 初始化

---
+++ src/main/java/pro/kwongsui/leetcode/App.java
@@ -0,0 +1,13 @@
+package pro.kwongsui.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

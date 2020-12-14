d993127cd7aa1d86b7b73609ea45f07a89447bdc
kwongsui <hwangkwongsui@gmail.com>
2020-12-14 19:42:24 +0800

    UPDATE：修正空数组转换错误

--- src/test/java/pro/kwongsui/leetcode/util/IntArrayConverter.java
+++ src/test/java/pro/kwongsui/leetcode/util/IntArrayConverter.java
@@ -10,22 +10,17 @@ public class IntArrayConverter implements ArgumentConverter {
   public Object convert(Object source, ParameterContext context)
       throws ArgumentConversionException {
     if (!(source instanceof String)) {
-      throw new IllegalArgumentException(
-          "The argument should be a string: " + source);
+      throw new IllegalArgumentException("The argument should be a string: " + source);
     }
     try {
       if (((String) source).length() == 0) {
-        return new Integer[0];
+        return new int[0];
       }
       String[] ints = ((String) source).split(",");
-      Integer[] ret = new Integer[ints.length];
+      int[] ret = new int[ints.length];
       int idx = 0;
       for (String o : ints) {
-        if (o.equals("null")) {
-          ret[idx++] = null;
-        } else {
-          ret[idx++] = Integer.parseInt(o);
-        }
+        ret[idx++] = Integer.parseInt(o);
       }
       return ret;
     } catch (Exception e) {

113d12a7abc1dc278288857ff2b2fccb6b5a1997
kwongsui <hwangkwongsui@gmail.com>
2020-12-14 19:48:19 +0800

    UPDATE：新增解法并完善测试

--- src/main/java/pro/kwongsui/leetcode/hash/Solution0205.java
+++ src/main/java/pro/kwongsui/leetcode/hash/Solution0205.java
@@ -1,8 +1,34 @@
 package pro.kwongsui.leetcode.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution0205 {
 
+  /**
+   * 常规解法：哈希表映射s中字符到t中字符
+   *          额外考虑s中字符未映射到t中字符，而哈希表中已有被映射为t中字符的记录
+   */
   public boolean isIsomorphic(String s, String t) {
+    int length = s.length();
+    Map<Character, Character> map = new HashMap<>();
+    for (int i = 0; i < length; i++) {
+      if (map.containsKey(s.charAt(i))) {
+        if (map.get(s.charAt(i)) != t.charAt(i)) {
+          return false;
+        }
+      } else {
+        if (map.containsValue(t.charAt(i))) {
+          return false;
+        } else {
+          map.put(s.charAt(i), t.charAt(i));
+        }
+      }
+    }
+    return true;
+  }
+
+  public boolean isIsomorphic2(String s, String t) {
     for (int i = 0; i < s.length(); i++) {
       // 相应位置字符第一次出现位置一致，即可相互映射，并保留顺序
       if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
--- src/test/java/pro/kwongsui/leetcode/hash/Solution0205Test.java
+++ src/test/java/pro/kwongsui/leetcode/hash/Solution0205Test.java
@@ -1,26 +1,27 @@
 package pro.kwongsui.leetcode.hash;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Solution0205Test {
 
-  Solution0205 solution = new Solution0205();
 
-  @Test
-  void isIsomorphic() {
-    Assertions.assertTrue(solution.isIsomorphic("", ""));
-    Assertions.assertTrue(solution.isIsomorphic("a", "b"));
-    Assertions.assertFalse(solution.isIsomorphic("aa", "ab"));
-    Assertions.assertFalse(solution.isIsomorphic("ab", "aa"));
-    Assertions.assertFalse(solution.isIsomorphic("aaaa", "aabb"));
-    Assertions.assertFalse(solution.isIsomorphic("abbbb", "ababb"));
-    Assertions.assertFalse(solution.isIsomorphic("aba", "baa"));
-    Assertions.assertTrue(solution.isIsomorphic("egg", "add"));
-    Assertions.assertFalse(solution.isIsomorphic("foo", "bar"));
-    Assertions.assertTrue(solution.isIsomorphic("paper", "title"));
+  @ParameterizedTest
+  @CsvSource({
+      "'','', 'true'",
+      "'a', 'b', 'true'",
+      "'aa', 'ab', 'false'",
+      "'ab', 'aa', 'false'",
+      "'aaaa', 'aabb', 'false'",
+      "'abbbb', 'ababb', 'false'",
+      "'aba', 'baa', 'false'",
+      "'egg', 'add', 'true'",
+      "'foo', 'bar', 'false'",
+      "'paper', 'title', 'true'"
+  })
+  void isIsomorphic(String s, String t, boolean expected) {
+    assertEquals(expected, new Solution0205().isIsomorphic(s, t));
   }
 }

6867b99520e77f897bfcd8401b06760a680c8e93
kwongsui <hwangkwongsui@gmail.com>
2021-01-03 08:02:29 +0800

    UPDATE：完善测试用例

--- src/test/java/pro/kwongsui/leetcode/hash/Solution0204Test.java
+++ src/test/java/pro/kwongsui/leetcode/hash/Solution0204Test.java
@@ -1,19 +1,26 @@
 package pro.kwongsui.leetcode.hash;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Solution0204Test {
 
   Solution0204 solution = new Solution0204();
 
-  @Test
-  void countPrimes() {
-    Assertions.assertEquals(0,solution.countPrimes(0));
-    Assertions.assertEquals(0, solution.countPrimes(1));
-    Assertions.assertEquals(0, solution.countPrimes(2));
-    Assertions.assertEquals(1, solution.countPrimes(3));
-    Assertions.assertEquals(4,solution.countPrimes(10));
-    Assertions.assertEquals(25, solution.countPrimes(100));
+  @ParameterizedTest
+  @CsvSource({
+      "0, 0",
+      "1, 0",
+      "2, 0",
+      "3, 1",
+      "10, 4",
+      "100, 25",
+      "1000, 168",
+  })
+  void countPrimes(int input, int expected) {
+    assertEquals(expected, new Solution0204().countPrimes(input));
   }
 }

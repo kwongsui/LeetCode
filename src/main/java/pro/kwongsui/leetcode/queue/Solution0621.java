4c6177487398e3eb97200082fb79039d52265bb7
kwongsui <hwangkwongsui@gmail.com>
2021-01-29 16:18:17 +0800

    UPDATE：代码整理

--- src/main/java/pro/kwongsui/leetcode/queue/Solution0621.java
+++ src/main/java/pro/kwongsui/leetcode/queue/Solution0621.java
@@ -9,53 +9,6 @@ import java.util.Queue;
 
 public class Solution0621 {
 
-    public int leastInterval(char[] tasks, int n) {
-        if (tasks.length <= 1 || n == 0) {
-            return tasks.length;
-        }
-
-        Map<Character, Task> map = new HashMap<>();
-        for (char t : tasks) {
-            map.putIfAbsent(t, new Task(t, 0));
-            map.get(t).freq++;
-        }
-        PriorityQueue<Task> priorityQueue = new PriorityQueue<>((t1, t2) -> t2.freq - t1.freq);
-        priorityQueue.addAll(map.values());  // 任务次数多优先级高，待执行任务队列
-
-        int number = 0;
-        Queue<Task> queue = new LinkedList<>();  // 冷却中任务队列
-
-//        Queue<Task> ret = new LinkedList<>(); // 最终执行任务序列
-        while (!priorityQueue.isEmpty() || !queue.isEmpty()) {
-            // 优先执行需执行次数最多的任务
-            Task t = priorityQueue.poll();
-            Objects.requireNonNull(t).last = number++;
-            t.freq--;
-//            ret.offer(t);
-            // 任务仍需执行，进入冷却中任务队列
-            if (t.freq != 0) {
-                queue.offer(t);
-            }
-            // 若没有任务可执行，进入冷却状态，直到第一个冷却状态的任务解冻继续执行
-            if (priorityQueue.isEmpty() && !queue.isEmpty()) {
-                number = queue.peek().last + n + 1;
-            }
-//            if (priorityQueue.isEmpty()) {
-//                while (!queue.isEmpty() && number < queue.peek().last + n + 1) {
-//                    number++;
-//                    ret.offer(new Task('#', 0));
-//                }
-//            }
-            // 冷却状态的任务解冻后进入待执行任务队列
-            while (!queue.isEmpty() && number > queue.peek().last + n) {
-                priorityQueue.offer(queue.poll());
-            }
-        }
-
-        return number;
-//        return ret.size();
-    }
-
     /**
      * 相同任务之间必须间隔 n 个单位时间。
      * 贪心算法：求得最少 idle 时间的个数。
@@ -69,7 +22,7 @@ public class Solution0621 {
      *    3.4 若 n < cnt - 1，idle 时间个数小于0，此时区间长度实际为0，idle 为0。
      * 4. 总的执行时间：idle 个单位时间 + 任务总数
      */
-    public int leastInterval2(char[] tasks, int n) {
+    public int leastInterval(char[] tasks, int n) {
         if (tasks.length <= 1 || n == 0) {
             return tasks.length;
         }
@@ -92,6 +45,44 @@ public class Solution0621 {
         int availableTasks = tasks.length - m * cnt;
         int idles = Math.max(0, emptySlots - availableTasks);
         return tasks.length + idles;
+    }
+
+    public int leastInterval1(char[] tasks, int n) {
+        if (tasks.length <= 1 || n == 0) {
+            return tasks.length;
+        }
+
+        Map<Character, Task> map = new HashMap<>();
+        for (char t : tasks) {
+            map.putIfAbsent(t, new Task(t, 0));
+            map.get(t).freq++;
+        }
+        PriorityQueue<Task> priorityQueue = new PriorityQueue<>((t1, t2) -> t2.freq - t1.freq);
+        priorityQueue.addAll(map.values());  // 任务次数多优先级高，待执行任务队列
+
+        int number = 0;
+        Queue<Task> queue = new LinkedList<>();  // 冷却中任务队列
+
+        while (!priorityQueue.isEmpty() || !queue.isEmpty()) {
+            // 优先执行需执行次数最多的任务
+            Task t = priorityQueue.poll();
+            Objects.requireNonNull(t).last = number++;
+            t.freq--;
+            // 任务仍需执行，进入冷却中任务队列
+            if (t.freq != 0) {
+                queue.offer(t);
+            }
+            // 若没有任务可执行，进入冷却状态，直到第一个冷却状态的任务解冻继续执行
+            if (priorityQueue.isEmpty() && !queue.isEmpty()) {
+                number = queue.peek().last + n + 1;
+            }
+            // 冷却状态的任务解冻后进入待执行任务队列
+            while (!queue.isEmpty() && number > queue.peek().last + n) {
+                priorityQueue.offer(queue.poll());
+            }
+        }
+
+        return number;
     }
 
     static class Task {

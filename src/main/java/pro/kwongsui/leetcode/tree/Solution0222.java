8e81da08d2e2408dabedbccd26e0d855286ea4e6
kwongsui <hwangkwongsui@gmail.com>
2021-02-08 08:35:07 +0800

    UPDATE：修正错误的方法引用

--- src/main/java/pro/kwongsui/leetcode/tree/Solution0222.java
+++ src/main/java/pro/kwongsui/leetcode/tree/Solution0222.java
@@ -64,8 +64,8 @@ public class Solution0222 {
         if (root == null) {
             return 0;
         }
-        int l = countNodes(root.left);
-        int r = countNodes(root.right);
+        int l = countNodes1(root.left);
+        int r = countNodes1(root.right);
         return l + r + 1;
     }
 }

34690c06cb277e012b601c92317ab7e525879556
kwongsui <hwangkwongsui@gmail.com>
2021-03-12 18:49:23 +0800

    ADD：修改方法名使表意更清晰

--- src/main/java/pro/kwongsui/offer/Solution13.java
+++ src/main/java/pro/kwongsui/offer/Solution13.java
@@ -5,21 +5,21 @@ public class Solution13 {
     public int movingCount(int m, int n, int k) {
         int[] res = new int[]{0};
         boolean[][] visited = new boolean[m][n];
-        backtrack(0, 0, m, n, k, visited, res);
+        dfs(0, 0, m, n, k, visited, res);
         return res[0];
     }
 
-    private void backtrack(int i, int j, int m, int n, int k, boolean[][] visited, int[] res) {
+    private void dfs(int i, int j, int m, int n, int k, boolean[][] visited, int[] res) {
         if(0 <= i && i < m && 0 <= j && j < n && sum (i, j) <= k && !visited[i][j]) {
             res[0]++;
         } else {
             return;
         }
         visited[i][j] = true;
-        backtrack(i - 1, j, m, n, k, visited, res);
-        backtrack(i + 1, j, m, n, k, visited, res);
-        backtrack(i, j - 1, m, n, k, visited, res);
-        backtrack(i, j + 1, m, n, k, visited, res);
+        dfs(i - 1, j, m, n, k, visited, res);
+        dfs(i + 1, j, m, n, k, visited, res);
+        dfs(i, j - 1, m, n, k, visited, res);
+        dfs(i, j + 1, m, n, k, visited, res);
     }
 
     private int sum(int i, int j) {

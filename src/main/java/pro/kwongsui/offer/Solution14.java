437c147e4127754dd102c52c15f6251d0f7dd122
kwongsui <hwangkwongsui@gmail.com>
2021-03-13 08:41:42 +0800

    ADD：剪绳子

---
+++ src/main/java/pro/kwongsui/offer/Solution14.java
@@ -0,0 +1,15 @@
+package pro.kwongsui.offer;
+
+public class Solution14 {
+
+    public int cuttingRope(int n) {
+        if(n < 4)
+            return n - 1;
+        int res = 1;
+        while(n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        return res * n;
+    }
+}
---
+++ src/test/java/pro/kwongsui/offer/Solution14Test.java
@@ -0,0 +1,22 @@
+package pro.kwongsui.offer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class Solution14Test {
+
+    @ParameterizedTest
+    @CsvSource({
+        "2, 1",
+        "3, 2",
+        "4, 4",
+        "5, 6",
+        "10, 36",
+        "25, 8748"
+    })
+    void cuttingRope(int input, int expected) {
+        assertEquals(expected, new Solution14().cuttingRope(input));
+    }
+}

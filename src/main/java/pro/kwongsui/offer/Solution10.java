976b6a064ded13a7c17b9e0772b967ce6c75589c
kwongsui <hwangkwongsui@gmail.com>
2021-03-13 08:43:18 +0800

    ADD：合并代码

--- src/main/java/pro/kwongsui/offer/Solution10.java
+++ src/main/java/pro/kwongsui/offer/Solution10.java
@@ -1,6 +1,6 @@
 package pro.kwongsui.offer;
 
-public class Solution101 {
+public class Solution10 {
 
     public int fib(int n) {
         int[] memo = new int[n + 1];
@@ -40,5 +40,38 @@ public class Solution101 {
             f1 = fib;
         }
         return fib;
+    }
+
+    public int numWays(int n) {
+        int n0, n1 = 0, n2 = 1;
+        for(int i = 1; i <= n; i++) {
+            n0 = n1;
+            n1 = n2;
+            n2 = n0 + n1;
+            if(n2 > 1000000007)
+                n2 %= 1000000007;
+        }
+        return n2;
+    }
+
+    public int numWays1(int n) {
+        int[] memo = new int[n + 1];
+        return numWays(n, memo);
+    }
+
+    private int numWays(int n, int[] memo) {
+        if(n == 0)
+            return 1;
+        else if(n == 1)
+            return 1;
+        else if(n == 2)
+            return 2;
+        else if(memo[n] > 0)
+            return memo[n];
+
+        memo[n] = numWays(n - 1, memo) + numWays(n - 2, memo);
+        if(memo[n] > 1000000007)
+            memo[n] %= 1000000007;
+        return memo[n];
     }
 }
--- src/test/java/pro/kwongsui/offer/Solution10Test.java
+++ src/test/java/pro/kwongsui/offer/Solution10Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class Solution101Test {
+class Solution10Test {
 
     @ParameterizedTest
     @CsvSource({
@@ -17,6 +17,18 @@ class Solution101Test {
         "100, 687995182"
     })
     void fib(int input, int expected) {
-        assertEquals(expected, new Solution101().fib(input));
+        assertEquals(expected, new Solution10().fib(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1",
+        "1, 1",
+        "2, 2",
+        "7, 21",
+        "100, 782204094"
+    })
+    void numWays(int input, int expected) {
+        assertEquals(expected, new Solution10().numWays(input));
     }
 }

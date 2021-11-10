c521fcaa99f8f93dc95850204e0d49adebe588d6
kwongsui <hwangkwongsui@gmail.com>
2021-11-10 10:44:43 +0800

    UPDATE：新增矩阵快速幂实现

--- src/main/java/pro/kwongsui/leetcode/dp/Solution0509.java
+++ src/main/java/pro/kwongsui/leetcode/dp/Solution0509.java
@@ -3,6 +3,74 @@ package pro.kwongsui.leetcode.dp;
 public class Solution0509 {
 
     public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int[][] a = new int[2][2];
+        a[0][0] = a[0][1] = a[1][0] = 1;
+
+        int[][] base = new int[2][1];
+        base[0][0] = 1;
+
+        int[][] res = quickPow(a, n - 1);
+        res = multiply(res, base);
+
+        return res[0][0];
+    }
+
+    private int[][] multiply(int[][] a, int[][] b) {
+        int rows = a.length;
+        int columns = b[0].length;
+        int[][] c = new int[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                for (int k = 0; k < b.length; k++) {
+                    c[row][column] += a[row][k] * b[k][column];
+                }
+            }
+        }
+        return c;
+    }
+
+    private int[][] quickPow(int[][] x, int N) {
+        int[][] res = new int[x.length][x[0].length];
+        for (int i = 0; i < x.length; i++) {
+            res[i][i] = 1;
+        }
+
+        while (N != 0) {
+            if ((N & 1) == 1) {
+                res = multiply(res, x);
+            }
+            x = multiply(x, x);
+            N >>>= 1;
+        }
+
+        return res;
+    }
+
+    /**
+     * 快速幂算法，时间复杂度 logN
+     * @param x 底数
+     * @param N 幂指数
+     */
+    public int quickPow(int x, int N) {
+        int res = 1;
+        while (N != 0) {
+            if ((N & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            N >>>= 1;
+        }
+        return res;
+    }
+
+    public int fib2(int n) {
         int f0 = 0, f1 = 1, fib = 0;
         if (n == 0) {
             return f0;
--- src/test/java/pro/kwongsui/leetcode/dp/Solution0509Test.java
+++ src/test/java/pro/kwongsui/leetcode/dp/Solution0509Test.java
@@ -10,10 +10,23 @@ class Solution0509Test {
 
     @ParameterizedTest
     @CsvSource({
+        "0, 0",
+        "1, 1,",
+        "2, 1",
+        "3, 2",
         "4, 3",
         "30, 832040"
     })
     void fib(int input, int expected) {
         assertEquals(expected, new Solution0509().fib(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "2, 10, 1024",
+        "5, 5, 3125"
+    })
+    void quickPow(int input1, int input2, int expected) {
+        assertEquals(expected, new Solution0509().quickPow(input1, input2));
     }
 }
